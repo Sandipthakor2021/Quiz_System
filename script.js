2551c7a901ee6b90dc96a619f91f70786cfe9b34
Sandip <sandipthakor2708@gmail.com>
2023-12-19 19:15:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+// Define an array of quiz questions
+const quizQuestions = [
+    {
+      question: "What is the capital of France?",
+      options: ["Paris", "London", "Berlin", "Rome"],
+      correctAnswer: "Paris"
+    },
+    {
+      question: "Which planet is known as the Red Planet?",
+      options: ["Venus", "Mars", "Jupiter", "Saturn"],
+      correctAnswer: "Mars"
+    },
+    {
+      question: "What is the chemical symbol for gold?",
+      options: ["Au", "Ag", "Cu", "Fe"],
+      correctAnswer: "Au"
+    }
+  ];
+  
+  // Variables to track quiz state
+  let currentQuestionIndex = 0;
+  let score = 0;
+  let timeLeft = 30;
+  let timerInterval;
+  
+  // Function to start the quiz
+  function startQuiz() {
+    document.getElementById("start-button").style.display = "none";
+    displayQuestion();
+    startTimer();
+  }
+  
+  // Function to display a question and its options
+  function displayQuestion() {
+    const { question, options } = quizQuestions[currentQuestionIndex];
+    const questionText = document.getElementById("question-text");
+    const answerButtons = document.getElementById("answer-buttons");
+  
+    // Clear previous question and answer options
+    questionText.innerHTML = question;
+    answerButtons.innerHTML = "";
+  
+    // Create answer buttons for each option
+    options.forEach(option => {
+      const button = document.createElement("button");
+      button.innerText = option;
+      button.classList.add("answer-button");
+      answerButtons.appendChild(button);
+  
+      // Add click event listener to check the answer
+      button.addEventListener("click", () => checkAnswer(option));
+    });
+  }
+  
+  // Function to check the selected answer
+  function checkAnswer(selectedOption) {
+    if (selectedOption === quizQuestions[currentQuestionIndex].correctAnswer) {
+      score++;
+    }
+  
+    // Move to the next question or end the quiz if all questions are answered
+    currentQuestionIndex++;
+  
+    if (currentQuestionIndex < quizQuestions.length) {
+      displayQuestion();
+    } else {
+      endQuiz();
+    }
+  }
+  
+  // Function to start the timer
+  function startTimer() {
+    timerInterval = setInterval(() => {
+      timeLeft--;
+  
+      // Update the timer text
+      document.getElementById("timer").textContent = timeLeft;
+  
+      // End the quiz if time runs out
+      if (timeLeft <= 0) {
+        endQuiz();
+      }
+    }, 1000);
+  }
+  
+  // Function to end the quiz
+  function endQuiz() {
+    clearInterval(timerInterval);
+  
+    // Calculate the score percentage
+    const scorePercentage = (score / quizQuestions.length) * 100;
+  
+    // Display the final score
+    const questionContainer = document.getElementById("question-container");
+    questionContainer.innerHTML = `
+      <h2>Quiz Completed!</h2>
+      <p>Your Score: ${score} out of ${quizQuestions.length}</p>
+      <p>Score Percentage: ${scorePercentage.toFixed(2)}%</p>
+    `;
+  }
+  
+  // Add event listener to start the quiz when the start button is clicked
+  document.getElementById("start-button").addEventListener("click", startQuiz);
+  
